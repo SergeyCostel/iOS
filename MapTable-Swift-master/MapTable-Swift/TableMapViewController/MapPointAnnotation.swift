4d4e9f6f86c068c016ea6033384a8ee8a2b55c30
SergeyCostel <sergeycostel@yahoo.com>
2016-01-01 21:48:45 +0300

    Google Map

---
+++ MapTable-Swift-master/MapTable-Swift/TableMapViewController/MapPointAnnotation.swift
@@ -0,0 +1,23 @@
+//
+//  MapPointAnnotation.swift
+//  MapTable-Swift
+//
+//  Created by Gazolla on 29/07/14.
+//  Copyright (c) 2014 Gazolla. All rights reserved.
+//
+
+// Copyright belongs to original author
+// http://code4app.net (en) http://code4app.com (cn)
+// From the most professional code share website: Code4App.net
+
+import UIKit
+import MapKit
+
+class MapPointAnnotation : MKPointAnnotation {
+    
+    var venue:Venue?
+    
+    
+}
+
+
---
+++ MapTable-Swift-master/README.md
@@ -0,0 +1,13 @@
+MapTable-Swift
+==============
+
+Mimic Find My Friends first screen behavior 
+
+[Youtube Video](http://youtu.be/GlZ38GZgJLM)
+
+### Contact
+
+* Sebastian Gazolla Jr
+* [@gazollajr](http://twitter.com/gazollajr)
+* [http://gazapps.com](http://gazapps.com)
+* [http://about.me/gazolla](http://about.me/gazolla)
